b1fbf3f22bc8c71e7ff4a201af8c00387ea11e9c
DerpyNewbie <derpynewbie@gmail.com>
2019-09-13 08:46:05 +0900

    🎉 First commit

---
+++ src/main/java/com/github/derpynewbie/timedeconomy/TimedEconomy.java
@@ -0,0 +1,16 @@
+package com.github.derpynewbie.timedeconomy;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TimedEconomy extends JavaPlugin {
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+}

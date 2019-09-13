7de6881dfb0b4720e50020e0da58ffea389c7e85
DerpyNewbie <derpynewbie@gmail.com>
2019-09-13 09:08:05 +0900

    ✨ Add Vault-API economy

--- src/main/java/com/github/derpynewbie/timedeconomy/TimedEconomy.java
+++ src/main/java/com/github/derpynewbie/timedeconomy/TimedEconomy.java
@@ -1,8 +1,12 @@
 package com.github.derpynewbie.timedeconomy;
 
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class TimedEconomy extends JavaPlugin {
+
+    private static Economy econ = null;
 
     @Override
     public void onDisable() {
@@ -12,5 +16,26 @@ public class TimedEconomy extends JavaPlugin {
     @Override
     public void onEnable() {
         super.onEnable();
+        if (!setupEconomy()) {
+            getLogger().severe("Vault not found.");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+    }
+
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        econ = rsp.getProvider();
+        return econ != null;
+    }
+
+    public static Economy getEconomy() {
+        return econ;
     }
 }
